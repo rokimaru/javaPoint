677fd588f8c83746f64d9c2f08811e5a1c35b3e7
rokimaru <34518268+rokimaru@users.noreply.github.com>
2022-01-18 19:50:51 +0300

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -4,12 +4,16 @@ import static java.lang.Math.sqrt;
 public class Program {
 
     public static void main(String[] args) {
+        //Создаем два объекта класса Point - две точки на плоскости
         Point p1 = new Point(0, 0);
         Point p2 = new Point(10, 10);
+        //Вызываем функцию нахождения расстояния между точками
         System.out.println("Расстояние между точками (" + p1.toString() + ") и (" + p2.toString() + ") = " + distance(p1, p2));
-        System.out.println(p1.distance(p2));
+        //Вызываем метод нахождения расстояния между точками
+        System.out.println("Расстояние между точками (" + p1.toString() + ") и (" + p2.toString() + ") = " + p1.distance(p2));
     }
 
+    //Функция ахождения расстояния между точками
     public static double distance(Point p1, Point p2) {
         return sqrt((p2.getX() - p1.getX()) * (p2.getX() - p1.getX()) + (p2.getY() - p1.getY()) * (p2.getY() - p1.getY()));
     }
